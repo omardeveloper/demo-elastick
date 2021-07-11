c34aea0c1d5928813b4dfbe64f64009f2e9ca813
omardeveloper <omartecnodigital@hotmail.com>
2021-07-10 21:53:36 -0500

    first Commit

---
+++ src/main/java/com/demo/elastick/DemoElastickApplication.java
@@ -0,0 +1,13 @@
+package com.demo.elastick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElastickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoElastickApplication.class, args);
+	}
+
+}
